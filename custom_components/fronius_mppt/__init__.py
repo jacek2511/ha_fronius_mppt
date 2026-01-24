c103b6324e9381bb63b3d9720050fc827bdd25d0
jacek2511 <112733566+jacek2511@users.noreply.github.com>
2026-01-24 21:05:33 +0100

    Create __init__.py

---
+++ custom_components/fronius_mppt/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    from .sensor import FroniusDataUpdateCoordinator 
+    
+    ip = entry.data["ip_address"]
+    coordinator = FroniusDataUpdateCoordinator(hass, ip)
+    
+    await coordinator.async_config_entry_first_refresh()
+    
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
